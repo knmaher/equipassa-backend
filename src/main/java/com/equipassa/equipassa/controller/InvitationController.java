f6d6669e313229c8de0f4d2be6eb7f26dffb693c
knmaher <maher.k.naser@gmail.com>
2025-08-05 21:18:53 +0200

    chore: fix hey api by adding parameter annotation

--- src/main/java/com/equipassa/equipassa/controller/InvitationController.java
+++ src/main/java/com/equipassa/equipassa/controller/InvitationController.java
@@ -6,11 +6,15 @@ import com.equipassa.equipassa.security.CustomUserDetails;
 import com.equipassa.equipassa.security.dto.AcceptInviteRequest;
 import com.equipassa.equipassa.security.dto.InviteUserRequest;
 import com.equipassa.equipassa.security.service.InvitationService;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/invitations")
@@ -26,7 +30,7 @@ public class InvitationController {
     @PreAuthorize("hasRole('ORG_ADMIN')")
     public ResponseEntity<String> invite(
             @RequestBody @Valid final InviteUserRequest request,
-            @CurrentUser final CustomUserDetails admin
+            @CurrentUser @Parameter(hidden = true) final CustomUserDetails admin
     ) {
         final String token = invitationService.sendInvitation(request, admin.getId()).getToken();
         return ResponseEntity.status(HttpStatus.CREATED).body(token);
